491dc5df7c2838e95c2b626c2f9d08347323d150
Lucas Sutherland <sutherlandlucas1@gmail.com>
2023-05-08 15:48:09 +1000

    seperation into multiple files

--- adventure.c
+++ adventure.c
@@ -1,58 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-#define BOARD_ROWS 10
-#define BOARD_COLS 17
+#include "graphics.h"
+#include "game.h"
 
-enum color {
-    DEFAULT = 0,
-    BLACK = 30,
-    RED,
-    GREEN,
-    YELLOW,
-    BLUE,
-    MAGENTA,
-    CYAN,
-    LIGHT_GRAY,
-    DARK_GRAY = 90,
-    LIGHT_RED,
-    LIGHT_GREEN,
-    LIGHT_YELLOW,
-    LIGHT_BLUE,
-    LIGHT_MAGENTA,
-    LIGHT_CYAN,
-    WHITE,
-};
-
-// includes color mapping
-enum terrain {
-    VOID = DEFAULT,
-    GRASS = LIGHT_GREEN,
-    SAND = LIGHT_YELLOW,
-    WATER = LIGHT_CYAN,
-    MOUNTAIN = DARK_GRAY,
-    FOREST = GREEN,
-    OCEAN = LIGHT_BLUE,
-};
-
-enum entity {
-    NONE,
-    PLAYER,
-};
-
-struct tile {
-    enum terrain terrain;
-    enum entity entity;
-};
-
-void color_print(char *text, enum color color);
-void print_board(struct tile board[BOARD_ROWS][BOARD_COLS]);
 void generate_terrain(struct tile board[BOARD_ROWS][BOARD_COLS]);
-void print_tile(struct tile tile);
 void tile_terrain_decision(int row, int col, 
     struct tile board[BOARD_ROWS][BOARD_COLS]);
 void generate_entities(struct tile board[BOARD_ROWS][BOARD_COLS]);
-
+bool in_board(int row, int col);
 
 int main(void) {
 
@@ -83,14 +40,14 @@ int main(void) {
 
         board[player_row][player_col].entity = NONE;
 
-        if (move == 'w') player_row--;
-        else if (move == 'a') player_col--;
-        else if (move =='s') player_row++;
-        else if (move == 'd') player_col++;
+        if (move == 'w' && in_board(player_row - 1, player_col)) player_row--;
+        else if (move == 'a' && in_board(player_row, player_col - 1)) player_col--;
+        else if (move =='s' && in_board(player_row + 1, player_col)) player_row++;
+        else if (move == 'd' && in_board(player_row, player_col + 1)) player_col++;
 
         board[player_row][player_col].entity = PLAYER;
 
-        printf("\033[1;1H\033[2J");
+        clear_screen();
         print_board(board);
     }
 
@@ -128,25 +85,6 @@ void tile_terrain_decision(int row, int col,
 
 }
 
-void print_board(struct tile board[BOARD_ROWS][BOARD_COLS]) {
-    for(int row = 0; row < BOARD_ROWS; row++) {
-        for(int col = 0; col < BOARD_COLS; col++) {
-            print_tile(board[row][col]);
-        }
-        printf("\n");
-    }
-}
-
-void print_tile(struct tile tile) {
-    if (tile.entity == PLAYER) {
-        color_print("\u3000", RED); // regular spaces don't work, ideographic space does
-    } else {
-        color_print("\u3000", tile.terrain); // regular spaces don't work, ideographic space does
-    }
-}
-
-void color_print(char *text, enum color color) {
-    // see ansi color codes for fun
-    printf("\033[7m");          // makes fore/background reverse, bit scuffed w whitespace
-    printf("\033[%dm%s\033[0m", color, text); // sets color, prints text, reverts to default style
+bool in_board(int row, int col) {
+    return row >= 0 && row < BOARD_ROWS && col >= 0 && col < BOARD_COLS;
 }
